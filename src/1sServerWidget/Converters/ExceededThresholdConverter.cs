3080e1d5e085dc9c6b95c5b58bce488c352bea2c
dj_serega <galyuk.sergey@gmail.com>
2018-06-20 00:26:27 +0300

    highlighting cell values in several steps

---
+++ src/1sServerWidget/Converters/ExceededThresholdConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace _1sServerWidget
+{
+    public class ExceededThresholdConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            float floatValue = ToFloat(value);
+
+            if (floatValue >= 15)
+                return new SolidColorBrush(Colors.Red);
+            else if (floatValue >= 10)
+                return new SolidColorBrush(Colors.Yellow);
+            else if (floatValue >= 5)
+                return new SolidColorBrush(Colors.LimeGreen);
+            else
+                return new SolidColorBrush();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new SolidColorBrush();
+        }
+
+        private float ToFloat(object value)
+        {
+            return System.Convert.ToSingle(value);
+        }
+    }
+}
